bc2ab566bafbd4491dbe0895ab984c7faad495fa
ececi <huseyinkiceci@gmail.com>
2021-10-29 16:35:41 +0300

    project started

---
+++ src/main/java/com/lespania/carpetcostcalculator/CarpetCostCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.lespania.carpetcostcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpetCostCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpetCostCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lespania/carpetcostcalculator/CarpetCostCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lespania.carpetcostcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpetCostCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
