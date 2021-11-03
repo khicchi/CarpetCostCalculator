f7b8b7e972778401601bc7addcd705a9d3991051
ececi <huseyinkiceci@gmail.com>
2021-11-03 22:09:39 +0300

    carpet info created

--- src/main/java/com/lespania/carpetcostcalculator/CarpetCostCalculatorApplication.java
+++ src/main/java/com/lespania/carpetcostcalculator/CarpetCostCalculatorApplication.java
@@ -1,47 +1,26 @@
 package com.lespania.carpetcostcalculator;
 
-import com.lespania.enums.EStates;
-import com.lespania.services.CarpetTX;
-import com.lespania.services.CarpetVA;
+import com.lespania.interfaces.ICarpetPrices;
+import com.lespania.services.carpet.CarpetTX;
+import com.lespania.services.carpet.CarpetVA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 @SpringBootApplication(scanBasePackages = "com.lespania")
+@ComponentScan({"com.lespania.enums", "com.lespania.services"})
 public class CarpetCostCalculatorApplication {
 
     public static void main(String[] args) {
         ApplicationContext container = SpringApplication.run(CarpetCostCalculatorApplication.class, args);
 
-         container.getBean("carpetTX", CarpetTX.class);
-         container.getBean("carpetVA", CarpetVA.class);
 
     }
 
-    private static void carpetCostProcess(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("From which state do you want to buy a carpet? TX or VA");
-        String stateInput = scanner.nextLine();
-
-        System.out.println("From which city do you want to buy a carpet?");
-        String cityInput = scanner.nextLine();
-
-        System.out.println("Is it a kitchen or bedroom or living room?");
-        String floorInput = scanner.nextLine();
-
-
-    }
-
-    private static void checkState(String stateInput){
-        //if user input is in the states enum
-        if (Arrays.stream(EStates.values()).map(EStates::getAbbreviation).collect(Collectors.toList()).contains(stateInput)){
-
-        }
-    }
 
 }
--- src/main/java/com/lespania/enums/ECities.java
+++ src/main/java/com/lespania/enums/ECities.java
@@ -1,21 +1,14 @@
 package com.lespania.enums;
 
+import org.springframework.stereotype.Component;
+
 public enum ECities {
 
-    city_dallas(40),
-    city_austin(20),
-    city_sanAntonio(30),
-    city_arlington(33),
-    city_fairfax(21),
-    city_mcLean(28);
+    DALLAS,
+    AUSTIN,
+    SANANTONIO,
+    ARLINGTON,
+    FAIRFAX,
+    MCLEAN;
 
-    private double unitePrice;
-
-    ECities(double unitePrice){
-        this.unitePrice = unitePrice;
-    }
-
-    public double getUnitePrice() {
-        return unitePrice;
-    }
 }
